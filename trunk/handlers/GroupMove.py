769c60c25f883047ecfe701e3f73d4e804c6f14b
plunchete <plunchete@8c6309cb-6455-0410-8621-9ebd31e19198>
2008-10-05 17:41:36 +0000

    Cambios en el movimiento de grupos.

--- trunk/handlers/GroupMove.py
+++ trunk/handlers/GroupMove.py
@@ -3,6 +3,7 @@
 
 #
 # (C) Copyright 2008 Juan Luis Belmonte  <jlbelmonte at gmail dot com>
+# (C) Copyright 2008 Ignacio Andreu <plunchete at gmail dot com>
 # 
 # This file is part of "debug_mode_on".
 # 
@@ -19,7 +20,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with "debug_mode_on".  If not, see <http://www.gnu.org/licenses/>.
 # 
-
 from google.appengine.ext import db
 from handlers.AuthenticatedHandler import *
 
@@ -28,67 +28,132 @@ class GroupMove(AuthenticatedHandler):
 	def execute(self):
 		method = self.request.method
 		user = self.values['user']
-
+		if user.rol != 'admin':
+			self.forbidden()
+			return
+			
 		if method == 'GET':
-			if user.rol != 'admin':
-				self.forbidden()
-				return
-			group_orig = model.Group.get(self.get_param('key_orig'))
-			self.values['key_orig'] = group_orig.key
-			self.render('templates/group-move.html')	
+			action = self.get_param('action')
+			key = self.get_param('key_orig')
+			if key:
+				group_orig = model.Group.get(key)
+				self.values['key_orig'] = group_orig.key
+			self.render('templates/group-move.html')
+			return
 		else:
-			if user.rol != 'admin':
-				self.forbidden()
+			action = action = self.get_param('action')
+			if not action:
+				self.render('templates/group-move.html')
 				return
-			
-			group_orig = model.Group.get(self.get_param('key_orig'))
-			self.values['key_orig'] = group_orig.key
-			
-			try:
-				group_dest = model.Group.get(self.get_param('key_dest'))
-			except :
-				self.render('templates/group-move-result.html')
+			key_orig = self.get_param('key_orig')
+			if key_orig:
+				group_orig = model.Group.get(key_orig)
+			key_dest = self.get_param('key_dest')
+			if key_dest:
+				group_dest = model.Group.get(key_dest)
+			mesagge = ''
+			if not group_orig:
+				message = 'No existe el grupo origen'
+				self.values['message'] = message
+				self.render('templates/group-move.html')
 				return
-
-			for i in group_orig.groupitem_set:
-				deleted=False	
-				for j in group_dest.groupitem_set:
-					if i.item.key() == j.item.key():
-						i.delete()
-						deleted=True
-				if not deleted:
-					i.group = group_dest
-					group_dest.items +=1
-					i.put()
-								
-			for i in group_orig.groupuser_set:
-				deleted=False
-				for j in group_dest.groupuser_set:
-					if i.user.key() == j.user.key():
-				 		i.delete()
-						deleted=True
-				if not deleted:
-					i.group = group_dest
-					group_dest.members +=1
-					i.put()
-
-			for i in group_orig.thread_set:
-				i.group=group_dest
-				group_dest.threads += 1
-				i.put()
-			
-			group_dest.put()
-			group_orig.delete()
-			
-			app = model.Application.all().get()		
-			if app:
-				app.groups -= 1
-				app.put()
-				memcache.delete('app')
-			
-			self.values['move_ok'] = True
-
-			memcache.delete('index_groups')
-			memcache.delete('index_threads')
-		self.render('templates/group-move-result.html')	
+			elif not group_dest:
+				message = 'No existe el grupo destino'
+				self.values['message'] = message
+				self.render('templates/group-move.html')
+				return
+			elif action == 'mu':
+				#move users
+				message = self.move_users(group_orig, group_dest)
+			elif action == 'mt':
+				#move threads
+				message = self.move_threads(group_orig, group_dest)
+				memcache.delete('index_threads')
+			elif action == 'mi':
+				#move items
+				message = self.move_items(group_orig, group_dest)
+			elif action == 'delete':
+				message = self.delete_group(group_orig)
+				memcache.delete('index_groups')
+			if action != 'delete':
+				self.values['key_orig'] = group_orig.key
+				self.values['key_dest'] = group_dest.key
+			self.values['message'] = message
+			self.render('templates/group-move.html')
+			return 
 	
+	def move_items(self, group_orig, group_dest):
+		group_items = model.GroupItem.all().filter('group', group_orig).fetch(10)
+		counter = 0
+		for group_item in group_items:
+			item_dest = model.GroupItem.all().filter('group', group_dest).filter('item', group_item.item).get()
+			if item_dest:
+				group_item.delete()
+			else:
+				group_item.group = group_dest
+				group_item.group_title = group_dest.title
+				group_item.group_url_path = group_dest.url_path
+				group_item.put()
+				group_dest.items += 1
+				group_dest.put()
+				counter +=1
+			group_orig.items -= 1
+			group_orig.put()
+		
+		return 'Movidos %s items. Quedan %s en el grupo origen.' % (counter, group_orig.items)
+		
+	def move_threads(self, group_orig, group_dest):
+		group_thread = model.Thread.all().filter('group', group_orig).filter('parent_thread', None).fetch(1)[0]
+		group_thread.group = group_dest
+		group_thread.group_url_path = group_dest.url_path
+		group_thread.group_title = group_dest.title
+		responses = model.Thread.all().filter('parent_thread', group_thread)
+		counter = 0
+		if responses:
+			for response in responses:
+				response.group = group_dest
+				response.group_url_path = group_dest.url_path
+				response.group_title = group_dest.title
+				response.put()
+				counter +=1
+		
+		group_thread.put()
+		group_orig.threads -= 1
+		group_orig.put()
+		group_dest.threads += 1
+		group_dest.put()
+		return 'Movido un hilo con %s comentarios. Quedan %s hilos.' % (counter, group_orig.threads)
+		
+		
+	def move_users(self, group_orig, group_dest):
+		group_users = model.GroupUser.all().filter('group', group_orig).fetch(10)
+		counter = 0
+		for group_user in group_users:
+			user_dest = model.GroupUser.all().filter('group', group_dest).filter('user', group_user.user).get()
+			group_user.user.groups -= 1
+			group_user.user.put()
+			if user_dest:
+				group_user.delete()
+			else:
+				group_user.group = group_dest
+				group_dest.members += 1
+				group_dest.subscribers.append(group_user.user.email)
+				group_dest.put()
+				counter += 1
+				group_user.group_title = group_dest.title
+				group_user.group_url = group_dest.url_path
+				group_user.put()
+			
+			group_orig.members -= 1
+			group_orig.put()
+
+		return 'Movidos %s usuarios. Quedan %s en el grupo origen.' % (counter, group_orig.members)
+	
+	def delete_group(self, group_orig):
+		group_orig.delete()
+		app = self.get_application()
+		if app:
+			app.groups -= 1
+			app.put()
+			memcache.delete('app')
+		return 'Grupo borrado. Existen %s grupos.' % (app.groups)
