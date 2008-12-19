a092fdb4f9f2fa0e8e5cff0db63679e797c79978
gimenete <gimenete@8c6309cb-6455-0410-8621-9ebd31e19198>
2008-12-19 17:33:00 +0000

    Fixed sessions in IE7

--- trunk/session.py
+++ trunk/session.py
@@ -15,12 +15,12 @@ class Session(object):
 	
 	def load(self):
 		string_cookie = os.environ.get('HTTP_COOKIE', '')
-		# string_cookie = 'dmo="1229551142;gimenete;9d5027fdb5f50aa35771bcd2eb12dc260a99f9cc"; expires=Wed, 17-Dec-2008 21:59:02 GMT; Path=/; HttpOnly'
+		string_cookie = string_cookie.replace(';', ':') # for old sessions
 		self.cookie = Cookie.SimpleCookie()
 		self.cookie.load(string_cookie)
 		if self.cookie.get(COOKIE_NAME):
 			value  = self.cookie[COOKIE_NAME].value
-			tokens = value.split(';')
+			tokens = value.split(':')
 			if len(tokens) != 3:
 				return False
 			else:
@@ -31,9 +31,11 @@ class Session(object):
 					self.time = tokens[0]
 					self.user = tokens[1]
 					self.auth = h
-					t = int(self.time) # TODO: catch
+					try:
+						t = int(self.time)
+					except:
+						return False
 					if t > int(time.time()):
-						# print self.cookie
 						return True
 		return False
 	
@@ -43,9 +45,10 @@ class Session(object):
 		params = [self.time, self.user, self.seed]
 		self.auth = self.hash(params)
 		params = [self.time, self.user, self.auth]
-		self.cookie[COOKIE_NAME] = ';'.join(params)
+		self.cookie[COOKIE_NAME] = ':'.join(params)
 		self.cookie[COOKIE_NAME]['expires'] = expire
-		print self.cookie
+		self.cookie[COOKIE_NAME]['path'] = '/'
+		print 'Set-Cookie: %s; HttpOnly' % (self.cookie.output().split(':', 1)[1].strip())
 	
 	def hash(self, params):
 		return sha.new(';'.join(params)).hexdigest()
@@ -53,16 +56,15 @@ class Session(object):
 	"""
 	def __str__(self):	
 		params = [self.time, self.user, self.auth]
-		self.cookie[COOKIE_NAME] = ';'.join(params)
+		self.cookie[COOKIE_NAME] = ':'.join(params)
 		self.cookie[COOKIE_NAME]['path'] = '/'
 		return self.cookie.output()
-	"""
 
-"""
 s = Session('1234')
+s.load()
 if s.load():
 	print s.auth
 	print s.user
 s.store('gimenete', 3200)
-print s
-"""
+
+	"""
